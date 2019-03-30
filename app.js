190d8beda088f167ff0aa7130b58142b05933b58
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-03-30 17:30:45 +0200

    server lesson

--- app.js
+++ app.js
@@ -2,24 +2,15 @@ var http = require('http');
 
 var fs = require('fs');
 
-var readStream = fs.createReadStream('./readMe.txt');
-var writeStream =  fs.createWriteStream('./writeMe.txt');
-
-readStream.on('data',function(chunk){
-    
-    console.log('chunk received:');
-    writeStream.write(chunk);
-});
-
-
-/*
 var server = http.createServer(function(req,res){
 console.log('url '+ req.url );
-res.writeHead(200, {'Content-Type':'text/plain'});
-res.end('hey man!');
+res.writeHead(200, {'Content-Type':'text/html'});
+
+var readStream = fs.createReadStream('./index.html');
+readStream.pipe(res);
+
 });
 
 server.listen(3000, '127.0.0.1');
 
 console.log('you are listening to por 3000');
-*/
