cf857ecaa30af01fdf57e2557b94bcab012c0126
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-04-01 17:46:07 +0200

    start for express

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var fs = require('fs');
-
-var server = http.createServer(function(req,res){
-console.log('url '+ req.url );
-res.writeHead(200, {'Content-Type':'text/html'});
-
-var readStream = fs.createReadStream('./index.html');
-readStream.pipe(res);
-
-});
-
-server.listen(3000, '127.0.0.1');
-
-console.log('you are listening to por 3000');
+app.listen(3000);
