42add758996c8145408922aa18cddf5fb993a6b3
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-04-02 00:25:50 +0200

    test

---
+++ stuff.js
@@ -0,0 +1,8 @@
+module.exports.counter = function(arr) {
+    return `array length is ${arr.length}`;
+};
+module.exports.adder = function(a, b) {
+    return `sum is ${a+b}`;
+};
+
+module.exports.pi = 3.142;
