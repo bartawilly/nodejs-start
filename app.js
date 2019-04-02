9376d9b2358524c2ea99575ce9b6eaf6b72a9f94
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-04-02 02:27:49 +0200

    express http request

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+    res.send('this is string');
+});
+app.get('/profile/:id', function(req, res) {
+    res.send('profile id is' + req.params.id);
+});
 app.listen(3000);
