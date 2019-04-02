a4f5be8f237d65a67d5aa9fb5c72f2654bc50c5d
bartawilly <ahmednasser.bartawilly@gmail.com>
2019-04-02 20:29:57 +0200

    start using ejs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('this is string');
+    res.sendFile(__dirname + '/index.html');
 });
-app.get('/profile/:id', function(req, res) {
-    res.send('profile id is' + req.params.id);
+app.get('/profile/:name', function(req, res) {
+    var data = {age:23 , job:'ninja'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 app.listen(3000);
